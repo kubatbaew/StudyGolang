3869c2df042c852cd06ef1c181615addbc2c951b
kubatbaew <kubatbaew@gmail.com>
2024-02-18 21:51:50 +0600

    lesson 6

---
+++ lesson_6/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Инкремент
+	//var i int
+	//i = i + i
+	//i += i
+	//i++
+
+	// Декремент
+	//var i int
+	//i = i - 1
+	//i -= i
+	//i--
+
+	// Базовый for
+
+	/*var i int
+	for i = 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	for j := 1; j < 10; j++ {
+		fmt.Println(j)
+	}*/
+
+	// For как while
+	/*sum := 1
+	for sum <= 100 {
+		fmt.Println(sum)
+		sum++
+	}*/
+
+	// Бесконечный for
+	for {
+		fmt.Println("Hello")
+	}
+}
